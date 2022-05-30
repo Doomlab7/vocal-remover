5618d5c80b57e50ddbd3ad45b0b984b6e38c0da5
Nicholas Payne <nic.payne@protonmail.com>
2022-05-30 06:39:27 -0500

    WIP: add initial oliver script

---
+++ olivet.py
@@ -0,0 +1,26 @@
+import subprocess
+from pathlib import Path
+
+home = Path("/tmp/olivet/Olivet Bible/Worship/Music")
+
+target = Path("/tmp/olivet/Olivet Bible/Worship/Instumental from Nic")
+
+for m in home.glob("**/*.mp3"):
+    _path = Path(target, m.relative_to(home))
+    print(_path)
+    # p = subprocess.Popen(f"python inference.py --input {m} --tta --gpu 0", shell=True)
+    p = subprocess.Popen(
+        [
+            "python",
+            "inference.py",
+            "--input",
+            str(m),
+            "--output_dir",
+            str(_path.parent),
+            "--tta",
+            "--gpu",
+            "0",
+        ]
+    )
+    p.wait()
+    break
