2de7794d99df71021da2c4ff062811144e0ddac1
pypeaday <github@pype.aleeas.com>
2023-12-12 14:03:18 -0600

    working

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
 import os
 from fastapi.responses import HTMLResponse
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi import Request, Depends
 import subprocess
 from fastapi import Form
 
@@ -15,32 +17,46 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Templates configuration
 templates = Jinja2Templates(directory="templates")
 
+# Assuming you have a function to get a FastAPI request instance, if not you can remove it.
+def get_request(request: Request):
+    return request
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/error/", response_class=HTMLResponse)
+async def error_page(request: Request):
+    return templates.TemplateResponse("error_page.html", {"request": request})
 
-@app.post("/submit_link/")
-async def submit_link(link: str = Form(...)):
-    # Run your Python script with the provided link as input
+
+from fastapi import Request
+
+@app.post("/submit_link/", response_class=HTMLResponse)
+async def submit_link(link: str = Form(...), request: Request = None):
     try:
+        if link is None:
+            raise ValueError("Link is missing")
+
         # Replace "your_script.py" with the actual name of your Python script
         result = subprocess.run(["python", "downloader/main.py", link], capture_output=True, text=True)
+        if result.returncode != 0:
+            raise ValueError("Error running Python script")
         output = result.stdout
         error = result.stderr
+        breakpoint()
     except Exception as e:
         output = None
         error = str(e)
 
- # You can customize the redirect URL based on the result of your script
+    # You can customize the reroute URL based on the result of your script
     reroute_url = os.environ.get("REROUTE_URL") if not error else "/error"
 
+    # Use inline HTML and client-side JavaScript for redirection
     return HTMLResponse(content=f"""
         <html>
             <head>
                 <title>FastAPI Submission Box</title>
-                <link rel="stylesheet" href="{{ url_for('static', path='styles.css') }}">
             </head>
             <body>
                 <script>
--- downloader/main.py
+++ downloader/main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from argparse import ArgumentParser
 from pathlib import Path
@@ -15,33 +16,56 @@ TARGET = os.environ.get("NC_TARGET")
 URL = os.environ.get("NC_URL")
 USER = os.environ.get("NC_USER")
 
+# Configure logging to both console and a file
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+# Reused paths
+downloads_path = Path("/home/nic/third-party/vocal-remover/downloads")
+raw_path = downloads_path / "raw"
+mp3s_path = downloads_path / "mp3s"
+to_upload_path = mp3s_path / "to-upload"
+uploaded_path = mp3s_path / "uploaded"
+
 if __name__ == "__main__":
+
+    raise SystemExit(1)
+    logger = logging.getLogger(__name__)
+
     parser = ArgumentParser()
     parser.add_argument("link")
     args = parser.parse_args()
     link = DownloadRequest(link=args.link)
-    # download
-    youget(link)
 
-    # run inference and convert each downloaded file (it's only one, doing it this way so I don't have to know the filename apriori)
-    for file in Path("/home/nic/third-party/vocal-remover/downloads/raw").glob("*.mp4"):
+    # Download
+    youget(link)
+    logger.info("Download complete")
+
+    # Process downloaded files
+    for file in raw_path.glob("*.mp4"):
         inference_data = InferenceRequest(filename=str(file))
         data = InferenceRequest(filename=str(file.with_suffix('')))
-        # run inference
+
+        # Run inference
         run_inference(inference_data)
-        # convert og mp4 to mp3
+
+        # Convert mp4 to mp3
         convert_mp4(data)
 
-    for file in Path("/home/nic/third-party/vocal-remover/downloads/raw").glob("*_Instruments.wav"):
+    for file in raw_path.glob("*_Instruments.wav"):
         data = InferenceRequest(filename=str(file.with_suffix('')))
-        # convert _instruments.wav to _instruments.mp3
+        # Convert _instruments.wav to _instruments.mp3
         convert_wav(data)
 
-    # move .mp3s to to_upload
-    for move_these in Path("/home/nic/third-party/vocal-remover/downloads/raw").glob("*.mp3"):
-        move_these.rename(Path("/home/nic/third-party/vocal-remover/downloads/mp3s/to-upload") / move_these.name)
+    # Move .mp3s to to_upload
+    for move_these in raw_path.glob("*.mp3"):
+        move_these.rename(to_upload_path / move_these.name)
 
-    # upload to nextcloud
+    # Upload to Nextcloud
     options = {
         "webdav_hostname": URL,
         "webdav_login": USER,
@@ -49,12 +73,14 @@ if __name__ == "__main__":
     }
     client = Client(options)
     client.verify = True
-    for file in Path("/home/nic/third-party/vocal-remover/downloads/mp3s/to-upload/").glob("*.mp3"):
+    for file in to_upload_path.glob("*.mp3"):
         client.upload_file(remote_path=f"/{TARGET}/{file.name}", local_path=str(file))
-        # move file from to-upload to uploaded
-        file.rename(Path("/home/nic/third-party/vocal-remover/downloads/mp3s/uploaded") / file.name)
+        logger.info(f"Uploaded: {file.name}")
+        # Move file from to-upload to uploaded
+        file.rename(uploaded_path / file.name)
 
-    # delete .mp4 and .wav in raw
-    for file in Path("/home/nic/third-party/vocal-remover/downloads/raw").glob("*"):
+    # Delete .mp4 and .wav in raw
+    for file in raw_path.glob("*"):
         if file.suffix == ".mp4" or file.suffix == ".wav":
             file.unlink()
+            logger.info(f"Deleted: {file.name}")
