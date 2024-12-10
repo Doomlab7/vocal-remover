48b8f417c6f845583a89993837543c9c0f9c722f
pypeaday <pypeaday@pype.dev>
2024-12-09 19:55:54 -0600

    update for splitting mp3s and uploading to under review

--- Justfile
+++ Justfile
@@ -1,10 +1,10 @@
 download-split-upload link: 
   echo "downloading yt video, converting, splitting, uploading"
-  python ./download_yt_split_upload.py.py {{ link }}
+  python ./download_yt_split_upload.py {{ link }}
 
 split-convert-upload-clean:
   echo "converting for church, splitting, uploading"
-  python ./scripts/split_convert_upload_instrumental_clean.py
+  /var/home/nic/projects/personal/vocal-remover/.venv/bin/python ./scripts/split_convert_upload_instrumental_clean.py
 
 docker:
   docker pull pypeaday/vocal-remover-app
--- scripts/split_convert_upload_instrumental_clean.py
+++ scripts/split_convert_upload_instrumental_clean.py
@@ -1,8 +1,10 @@
-"""Splits mp3s and just uploads the instrumentals to Olivet Bible Instrumentals
+"""Splits mp3s and just uploads them plus the instrumentals to the under review folder
 
 1. Download mp3 to split and put in downloads/just-split-convert/
 2. `just split-convert-upload-clean`
+3. uploads to Songs Under Review
 """
+
 import os
 from pathlib import Path
 
@@ -12,12 +14,20 @@ from automations import run_inference
 from webdav3.client import Client
 
 PASSWORD = os.environ.get("NC_PASSWORD")
-TARGET = os.environ.get("NC_INSTRUMENTALS_ONLY_TARGET")
+# TARGET = os.environ.get("NC_INSTRUMENTALS_ONLY_TARGET")
+TARGET = os.environ.get("NC_TARGET")
 URL = os.environ.get("NC_URL")
 USER = os.environ.get("NC_USER")
 
 if __name__ == "__main__":
-    for file in Path("/app/downloads/just-split-convert").glob("*.mp3"):
+    options = {
+        "webdav_hostname": URL,
+        "webdav_login": USER,
+        "webdav_password": PASSWORD,
+    }
+    client = Client(options)
+    client.verify = True
+    for file in Path("./downloads/just-split-convert").glob("*.mp3"):
         inference_data = InferenceRequest(filename=str(file))
         data = InferenceRequest(filename=str(file.with_suffix("")))
         # check if already done for some reason
@@ -26,7 +36,9 @@ if __name__ == "__main__":
             continue
         # run inference
         run_inference(inference_data)
-    for file in Path("/app/downloads/just-split-convert").glob("*_Instruments.wav"):
+        # upload the full song
+        client.upload_file(remote_path=f"/{TARGET}/{file.name}", local_path=str(file))
+    for file in Path("./downloads/just-split-convert").glob("*_Instruments.wav"):
         # check if file with .mp3 extension exists
         if file.name.endswith(".mp3"):
             print(f"Skipping: {file.name}")
@@ -34,18 +46,10 @@ if __name__ == "__main__":
         data = InferenceRequest(filename=str(file.with_suffix("")))
         # convert _instruments.wav to _instruments.mp3
         convert_wav(data)
-
-    options = {
-        "webdav_hostname": URL,
-        "webdav_login": USER,
-        "webdav_password": PASSWORD,
-    }
-    client = Client(options)
-    client.verify = True
     # breakpoint()
-    for file in Path("/app/downloads/just-split-convert/").glob("*_Instruments.mp3"):
+    for file in Path("./downloads/just-split-convert/").glob("*_Instruments.mp3"):
         client.upload_file(remote_path=f"/{TARGET}/{file.name}", local_path=str(file))
-    for file in Path("/app/downloads/just-split-convert/").glob("*.mp3"):
+    for file in Path("./downloads/just-split-convert/").glob("*.mp3"):
         file.unlink()
-    for file in Path("/app/downloads/just-split-convert/").glob("*.wav"):
+    for file in Path("./downloads/just-split-convert/").glob("*.wav"):
         file.unlink()
